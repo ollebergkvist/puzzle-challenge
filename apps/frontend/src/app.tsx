4469d719aa7d84ac6577209c26b1666c5d73d7db
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-14 16:05:58 -0700

    feat: implement logic and components

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,33 +1,164 @@
 // libs
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "preact/hooks";
+
+// components
+import {
+  Filter,
+  LoadingSpinner,
+  Login,
+  Navbar,
+  Orders,
+  Products,
+  Search,
+} from "./components";
+import { getUniqueValues } from "./libs/get-unique-values";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [queriedProducts, setQueriedProducts] = useState([]);
+  const [cachedProducts, setCachedProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+
+  const [activeView, setActiveView] = useState("All Products");
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [categories, setCategories] = useState([]);
+  const [titles, setTitles] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const isCached = cachedProducts.find((item) => item.query === query);
+
+    if (isCached) {
+      setQueriedProducts(isCached.products);
+
+      return;
+    }
+
+    const debouncedFilter = setTimeout(() => {
+      const filteredItems = products.filter((item) => {
+        if (item.title.toLowerCase().includes(query)) {
+          return true;
+        }
+      });
+
+      setCachedProducts([
+        ...cachedProducts,
+        { query, products: filteredItems },
+      ]);
+      setQueriedProducts(filteredItems);
+    }, 300);
+
+    return () => clearTimeout(debouncedFilter);
+  }, [query]);
+
+  useEffect(() => {
+    setCategories(getUniqueValues(products, "category"));
+    setTitles(getUniqueValues(products, "title"));
+  }, [products]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      const result = await response.json();
+
+      setProducts(result);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const inputHandler = (event) => {
+    const { value } = event.target;
+
+    setQuery(value.toLowerCase());
+  };
+
+  const handleAddItemToCart = (event) => {
+    const { value } = event.target;
+
+    setQuery(value.toLowerCase());
+  };
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleViewToggle = (view: string) => {
+    setActiveView(view);
+  };
+
+  const displayProducts = query ? queriedProducts : products;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar handleOnClick={handleViewToggle} />
+
+      {activeView === "All Products" || activeView === "My Orders" ? (
+        <div className="mt-[64px]">
+          <button onClick={() => handleViewToggle("All Products")}>
+            All Products
+          </button>
+
+          <button onClick={() => handleViewToggle("My Orders")}>
+            My Orders
+          </button>
+        </div>
+      ) : null}
+
+      <main className="flex py-[5rem] flex-col">
+        {activeView === "All Products" && (
+          <div>
+            <Search value={query} handleOnChange={inputHandler} />
+
+            <div className="flex">
+              <Filter
+                buttonTitle="Filter by title"
+                filterTitle="Title"
+                values={titles}
+              />
+
+              <Filter
+                buttonTitle="Filter by category"
+                filterTitle="Category"
+                values={categories}
+              />
+
+              {!loading ? (
+                <Products
+                  products={displayProducts}
+                  handleOnClick={handleAddItemToCart}
+                />
+              ) : (
+                <LoadingSpinner />
+              )}
+            </div>
+          </div>
+        )}
+
+        {activeView === "My Orders" && (
+          <>
+            {isAuthenticated ? (
+              <Orders />
+            ) : (
+              <p>Please log in to view your orders.</p>
+            )}
+          </>
+        )}
+
+        {activeView === "Login" && (
+          <>
+            <Login />
+          </>
+        )}
+      </main>
+    </div>
   );
 }
