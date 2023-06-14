0b596168a2d511e3a01d6953be1b1e931a45f7a9
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-14 16:02:44 -0700

    feat: add seed

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: "admin@example.com",
+    password: "password123",
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
