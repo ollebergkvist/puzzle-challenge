8e3119d4d5475c0d36515427fa2c8cbdf2f9e0bb
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-15 14:06:32 -0700

    feat: add login route

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,9 +2,12 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { Prisma, PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// utils
+import { comparePasswords } from "./utils/compare-passwords";
 
 export const createServer = () => {
   const app = express();
@@ -14,7 +17,51 @@ export const createServer = () => {
     .use(morgan("dev"))
     .use(express.urlencoded({ extended: true }))
     .use(express.json())
-    .use(cors())
+    .use(cors());
+  app
+    .post("/login", async (req, res) => {
+      const schema = z.object({
+        email: z.string().min(1),
+        password: z.string().email(),
+      });
+
+      const validatedBody = schema.safeParse(req.body);
+
+      if (!validatedBody.success) {
+        res.send(400).end(validatedBody.error);
+      }
+
+      const { email, password } = req.body;
+
+      const prisma = new PrismaClient();
+
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user) {
+          return res.status(404).json({ error: "User not found" });
+        }
+
+        const passwordsMatch = comparePasswords(password, user.password);
+
+        if (!passwordsMatch) {
+          return res.status(401).json({ error: "Invalid password" });
+        }
+
+        const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+          expiresIn: "1h",
+        });
+
+        return res.json({ token });
+      } catch (error) {
+        console.error("Error during login:", error);
+        return res.status(500).json({ error: "An unexpected error occurred" });
+      }
+    })
     .get("/message/:name", (req, res) => {
       return res.json({ message: `hello ${req.params.name}` });
     })
