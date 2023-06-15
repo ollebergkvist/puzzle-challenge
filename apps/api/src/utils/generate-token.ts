e2cd560eba277b065db410675349dd4aa60aec63
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-15 14:55:54 -0700

    feat: add util

---
+++ apps/api/src/utils/generate-token.ts
@@ -0,0 +1,12 @@
+// libs
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId: string) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
+    expiresIn: "1h",
+  });
+
+  return token;
+};
+
+export default generateToken;
