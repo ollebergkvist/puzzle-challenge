01da95104754d2f786790e08a3e89f29a05faf22
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-15 14:04:12 -0700

    feat: implement hashing during seeding

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,19 +1,27 @@
+// libs
 import { PrismaClient, Prisma } from "@prisma/client";
+
+// utils
+import { hashPassword } from "../utils/hash-password";
 
 const prisma = new PrismaClient();
 
 const userData: Prisma.UserCreateInput[] = [
   {
     email: "admin@example.com",
-    password: "password123",
+    password: "password",
   },
 ];
 
 async function main() {
   console.log(`Start seeding ...`);
   for (const u of userData) {
+    const hashedPassword = await hashPassword(u.password);
     const user = await prisma.user.create({
-      data: u,
+      data: {
+        ...u,
+        password: hashedPassword,
+      },
     });
     console.log(`Created user with id: ${user.id}`);
   }
