1457c99ece0234f00022419c3d5f0aef7dfba90b
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-16 08:54:58 -0700

    feat: update rating component

--- apps/frontend/src/components/UI/Rating.tsx
+++ apps/frontend/src/components/UI/Rating.tsx
@@ -1,43 +1,60 @@
-// types
-import type { JSX } from "preact/jsx-runtime";
+// libs
+import { useEffect, useState } from "preact/hooks";
+import axios from "axios";
 
-export const RatingComponent = ({ rating: number }): JSX.Element => {
-  const filledStars = Math.round(rating);
-  const maxStars = 5;
+interface RatingProps {
+  orderId: string;
+}
+
+export const Rating = ({ orderId }: RatingProps) => {
+  const [rating, setRating] = useState(0);
+
+  useEffect(() => {
+    const fetchRating = async () => {
+      try {
+        const response = await axios.get(`/orders/${orderId}`);
+        const { rating } = response.data;
+
+        setRating(rating);
+      } catch (error) {
+        console.error("Error fetching rating:", error);
+      }
+    };
+
+    fetchRating();
+  }, [orderId]);
+
+  const handleRatingClick = async (newRating) => {
+    try {
+      await axios.post(`/orders/${orderId}/rate`, { rating: newRating });
+      setRating(newRating);
+    } catch (error) {
+      console.error("Error submitting rating:", error);
+    }
+  };
 
   const renderStars = () => {
     const stars = [];
 
-    for (let i = 0; i < maxStars; i++) {
-      if (i < filledStars) {
-        stars.push(
-          <svg
-            key={i}
-            aria-hidden="true"
-            className="w-5 h-5 text-yellow-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-          </svg>
-        );
-      } else {
-        stars.push(
-          <svg
-            key={i}
-            aria-hidden="true"
-            className="w-5 h-5 text-gray-300 dark:text-gray-500"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Star</title>
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-          </svg>
-        );
-      }
+    for (let i = 1; i <= 5; i++) {
+      const starColor =
+        i <= rating ? "text-yellow-400" : "text-gray-300 dark:text-gray-500";
+
+      stars.push(
+        <svg
+          key={i}
+          aria-hidden="true"
+          className={`w-5 h-5 ${starColor}`}
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={() => handleRatingClick(i)}
+        >
+          <title>Star</title>
+
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      );
     }
 
     return stars;
