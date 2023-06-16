432efd74baee4289b6bb6d056f0695ff7fcc0210
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-15 18:27:37 -0700

    feat: update path

--- apps/api/src/__tests__/server.test.ts
+++ apps/api/src/__tests__/server.test.ts
@@ -5,19 +5,10 @@ import { createServer } from "../server";
 describe("server", () => {
   it("health check returns 200", async () => {
     await supertest(createServer())
-      .get("/healthz")
+      .get("/api/health")
       .expect(200)
       .then((res) => {
         expect(res.body.ok).toBe(true);
-      });
-  });
-
-  it("message endpoint says hello", async () => {
-    await supertest(createServer())
-      .get("/message/jared")
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toEqual({ message: "hello jared" });
       });
   });
 });
