892c73c71555c90acb9cec7f8509fb280d072aaf
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-19 10:30:50 -0700

    feat: type utils

--- apps/frontend/src/utils/calculate-total-price.tsx
+++ apps/frontend/src/utils/calculate-total-price.tsx
@@ -1,4 +1,7 @@
-export const calculateTotalPrice = (cartItems) =>
+// types
+import { CartItem } from "../types";
+
+export const calculateTotalPrice = (cartItems: CartItem[]) =>
   cartItems.reduce(
     (total, item) => total + item.product.price * item.quantity,
     0
--- apps/frontend/src/utils/format-date.tsx
+++ apps/frontend/src/utils/format-date.tsx
@@ -1,7 +1,7 @@
 // libs
 import dayjs from "dayjs";
 
-export const formatDate = (dateString) => {
+export const formatDate = (dateString: string) => {
   const date = dayjs(dateString);
   const formattedDate = date.format("MMMM DD YYYY");
 
