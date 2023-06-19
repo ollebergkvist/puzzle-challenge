8d093d7ebf34652c13e6816ad81938ca070f4810
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-18 18:30:43 -0700

    feat: add layout

---
+++ apps/frontend/src/layout/layout.tsx
@@ -0,0 +1,16 @@
+// types
+import type { ComponentChildren } from "preact";
+
+interface LayoutProps {
+  children: ComponentChildren;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <div className="max-w-7xl mx-auto">
+        <div className="px-6 mt-[64px] py-12 lg:py-24"> {children}</div>
+      </div>
+    </div>
+  );
+};
