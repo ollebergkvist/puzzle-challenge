3cf14985dd968578d63b459860fdd9fd5425c31d
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-19 16:44:16 -0700

    feat: add util function

--- apps/frontend/src/utils/api-urls.ts
+++ apps/frontend/src/utils/api-urls.ts
@@ -1,3 +1,14 @@
 export const getCartUrl = `${import.meta.env.VITE_API_URL}/cart/items`;
 export const createPaymentUrl = `${import.meta.env.VITE_API_URL}/orders/pay`;
 export const cancelOrderUrl = `${import.meta.env.VITE_API_URL}/orders/cancel`;
+
+export const createDynamicUrl = (orderId) => {
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const orderUrl = `${baseUrl}/orders/${orderId}`;
+
+  return {
+    getOrderUrl: orderUrl,
+    updateOrderUrl: orderUrl,
+    deleteOrderItemUrl: orderUrl,
+  };
+};
