2c546ce1de8d587d7e62a6e12a39d4383711832b
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-18 18:31:58 -0700

    feat: implement router and context

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,164 +1,38 @@
 // libs
-import { useEffect, useState } from "preact/hooks";
+import { Router, Route } from "preact-router";
+
+// context
+import { AuthProvider, ViewToggleProvider } from "./context";
+
+// layout
+import { Layout } from "./layout";
+
+// pages
+import { CartPage, CheckoutPage, HomePage, LoginPage } from "./pages";
 
 // components
-import {
-  Filter,
-  LoadingSpinner,
-  Login,
-  Navbar,
-  Orders,
-  Products,
-  Search,
-} from "./components";
-import { getUniqueValues } from "./libs/get-unique-values";
+import { Navbar } from "./components";
 
 export function App() {
-  const [products, setProducts] = useState([]);
-  const [queriedProducts, setQueriedProducts] = useState([]);
-  const [cachedProducts, setCachedProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState("");
-
-  const [activeView, setActiveView] = useState("All Products");
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const [categories, setCategories] = useState([]);
-  const [titles, setTitles] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const isCached = cachedProducts.find((item) => item.query === query);
-
-    if (isCached) {
-      setQueriedProducts(isCached.products);
-
-      return;
-    }
-
-    const debouncedFilter = setTimeout(() => {
-      const filteredItems = products.filter((item) => {
-        if (item.title.toLowerCase().includes(query)) {
-          return true;
-        }
-      });
-
-      setCachedProducts([
-        ...cachedProducts,
-        { query, products: filteredItems },
-      ]);
-      setQueriedProducts(filteredItems);
-    }, 300);
-
-    return () => clearTimeout(debouncedFilter);
-  }, [query]);
-
-  useEffect(() => {
-    setCategories(getUniqueValues(products, "category"));
-    setTitles(getUniqueValues(products, "title"));
-  }, [products]);
-
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products");
-      const result = await response.json();
-
-      setProducts(result);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const inputHandler = (event) => {
-    const { value } = event.target;
-
-    setQuery(value.toLowerCase());
-  };
-
-  const handleAddItemToCart = (event) => {
-    const { value } = event.target;
-
-    setQuery(value.toLowerCase());
-  };
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
-  const handleViewToggle = (view: string) => {
-    setActiveView(view);
-  };
-
-  const displayProducts = query ? queriedProducts : products;
-
   return (
     <div>
-      <Navbar handleOnClick={handleViewToggle} />
+      <AuthProvider>
+        <Navbar />
 
-      {activeView === "All Products" || activeView === "My Orders" ? (
-        <div className="mt-[64px]">
-          <button onClick={() => handleViewToggle("All Products")}>
-            All Products
-          </button>
+        <Layout>
+          <ViewToggleProvider>
+            <Router>
+              <Route exact path="/" component={HomePage} />
 
-          <button onClick={() => handleViewToggle("My Orders")}>
-            My Orders
-          </button>
-        </div>
-      ) : null}
+              <Route path="/login" component={LoginPage} />
 
-      <main className="flex py-[5rem] flex-col">
-        {activeView === "All Products" && (
-          <div>
-            <Search value={query} handleOnChange={inputHandler} />
+              <Route exact path="/cart" component={CartPage} />
 
-            <div className="flex">
-              <Filter
-                buttonTitle="Filter by title"
-                filterTitle="Title"
-                values={titles}
-              />
-
-              <Filter
-                buttonTitle="Filter by category"
-                filterTitle="Category"
-                values={categories}
-              />
-
-              {!loading ? (
-                <Products
-                  products={displayProducts}
-                  handleOnClick={handleAddItemToCart}
-                />
-              ) : (
-                <LoadingSpinner />
-              )}
-            </div>
-          </div>
-        )}
-
-        {activeView === "My Orders" && (
-          <>
-            {isAuthenticated ? (
-              <Orders />
-            ) : (
-              <p>Please log in to view your orders.</p>
-            )}
-          </>
-        )}
-
-        {activeView === "Login" && (
-          <>
-            <Login />
-          </>
-        )}
-      </main>
+              <Route path="/checkout" component={CheckoutPage} />
+            </Router>
+          </ViewToggleProvider>
+        </Layout>
+      </AuthProvider>
     </div>
   );
 }
