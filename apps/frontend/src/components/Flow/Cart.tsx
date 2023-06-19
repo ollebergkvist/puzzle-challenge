952e4e921745bb4170622c778a08068b726afafa
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-18 21:37:19 -0700

    feat: update components

--- apps/frontend/src/components/Flow/Cart.tsx
+++ apps/frontend/src/components/Flow/Cart.tsx
@@ -32,43 +32,91 @@ export const Cart = () => {
 
   return (
     <div className="min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Cart</h1>
-
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <>
-          <ul className="space-y-4">
-            {cartItems.map((item) => (
-              <li key={item.id} className="flex items-center space-x-4">
-                <img
-                  src={item.product.image}
-                  alt={item.product.title}
-                  className="w-16 h-16 object-contain"
-                />
+          <div className="h-screen">
+            <h1 className="mb-10 text-2xl text-center font-bold sm:text-start">
+              Cart Items
+            </h1>
 
-                <div>
-                  <p className="font-bold">{item.product.title}</p>
-                  <p>${priceFormatter(item.product.price)}</p>
-                  <p>Quantity: {item.quantity}</p>
+            <div className="mx-auto justify-center pb-12 px-6 md:flex md:space-x-6 xl:px-0">
+              <div className="md:w-2/3">
+                {cartItems.map((item) => (
+                  <div key={item.id} className="rounded-lg overflow-hidden">
+                    <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                      <img
+                        src={item.product.image}
+                        alt={item.product.title}
+                        className="w-full rounded-lg sm:w-40"
+                      />
+
+                      <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                        <div className="mt-5 sm:mt-0">
+                          <h2 className="text-sm font-bold text-gray-900">
+                            {item.product.title}
+                          </h2>
+                        </div>
+
+                        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                          <div className="flex items-center border-gray-100">
+                            <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                              {" "}
+                              -{" "}
+                            </span>
+
+                            <input
+                              className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                              type="number"
+                              value={item.quantity}
+                              min="1"
+                              disabled
+                            />
+
+                            <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                              {" "}
+                              +{" "}
+                            </span>
+                          </div>
+
+                          <div className="flex items-center space-x-4">
+                            <p className="text-sm">
+                              ${priceFormatter(item.product.price)}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                <div className="flex justify-between">
+                  <p className="text-lg font-bold">Total</p>
+
+                  <div className="">
+                    <p className="mb-1 text-lg font-bold">
+                      ${priceFormatter(totalPrice)}
+                    </p>
+                  </div>
                 </div>
-              </li>
-            ))}
-          </ul>
 
-          <p className="mt-4">Total Price: ${priceFormatter(totalPrice)}</p>
+                <button
+                  onClick={checkout}
+                  disabled={isCheckingOut}
+                  className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600"
+                >
+                  {isCheckingOut ? "Checking out..." : "Checkout"}
+                </button>
 
-          <button
-            className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-            onClick={checkout}
-            disabled={isCheckingOut}
-          >
-            {isCheckingOut ? "Checking out..." : "Checkout"}
-          </button>
-
-          {checkoutError && (
-            <div>Error during checkout: {checkoutError.message}</div>
-          )}
+                {checkoutError && (
+                  <div>Error during checkout: {checkoutError.message}</div>
+                )}
+              </div>
+            </div>
+          </div>
         </>
       )}
     </div>
--- apps/frontend/src/components/Flow/Orders.tsx
+++ apps/frontend/src/components/Flow/Orders.tsx
@@ -1,14 +1,23 @@
+// libs
+import { useState } from "react";
+
 // context
 import { useAuthContext } from "../../context";
 
 // hooks
-import { useGetOrders, usePayOrder, useRating } from "../../hooks";
+import {
+  useGetOrders,
+  usePayOrder,
+  useRating,
+  useCancelOrder,
+  useFilter,
+} from "../../hooks";
 
 // utils
 import { formatDate, priceFormatter } from "../../utils";
 
 // components
-import { LoadingSpinner, Rating } from "..";
+import { Filter, LoadingSpinner, Rating } from "..";
 
 // types
 import type { JSX } from "preact/jsx-runtime";
@@ -16,7 +25,24 @@ import type { JSX } from "preact/jsx-runtime";
 export const Orders = (): JSX.Element => {
   const { token } = useAuthContext();
 
-  const { orders, isLoading, error } = useGetOrders(token);
+  const [sortByPriceOrder, setSortByPriceOrder] = useState(null);
+
+  const {
+    selectedValues: selectedStatus,
+    handleFilterChange: handleStatusFilterChange,
+  } = useFilter();
+
+  const {
+    selectedValues: selectedRatings,
+    handleFilterChange: handleRatingsFilterChange,
+  } = useFilter();
+
+  const { orders, isLoading, error } = useGetOrders(
+    token,
+    selectedStatus,
+    selectedRatings,
+    sortByPriceOrder
+  );
 
   const {
     isLoading: isPaymentLoading,
@@ -26,9 +52,15 @@ export const Orders = (): JSX.Element => {
 
   const { updateRating } = useRating(token);
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+  const {
+    cancelOrder,
+    isLoading: isCancellingOrder,
+    error: cancelOrderError,
+  } = useCancelOrder(token);
+
+  // if (isLoading) {
+  //   return <LoadingSpinner />;
+  // }
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -36,10 +68,14 @@ export const Orders = (): JSX.Element => {
 
   const handleEditOrder = (event) => {
     const orderId = event.target.id;
+
+    // editOrder(orderId)
   };
 
   const handleCancelOrder = (event) => {
     const orderId = event.target.id;
+
+    cancelOrder(orderId);
   };
 
   const handlePayOrder = (event) => {
@@ -48,138 +84,199 @@ export const Orders = (): JSX.Element => {
     payOrder(orderId);
   };
 
+  const handleSortByPrice = () => {
+    if (sortByPriceOrder === null || sortByPriceOrder === "asc") {
+      setSortByPriceOrder("desc");
+    } else {
+      setSortByPriceOrder("asc");
+    }
+  };
+
+  const status = ["ACTIVE", "COMPLETED"];
+
+  const rating = ["0", "1", "2", "3", "4", "5"];
+
   return (
     <div className="min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Orders</h1>
 
-      {orders.length === 0 ? (
-        <p>No orders found.</p>
+      <div className="flex space-x-3 mt-4">
+        {selectedStatus && (
+          <Filter
+            buttonTitle="Filter by status"
+            filterTitle="Title"
+            values={status}
+            selectedValues={selectedStatus}
+            handleFilterChange={handleStatusFilterChange}
+          />
+        )}
+
+        {selectedRatings && (
+          <Filter
+            buttonTitle="Filter by rating"
+            filterTitle="Category"
+            selectedValues={selectedRatings}
+            values={rating}
+            handleFilterChange={handleRatingsFilterChange}
+          />
+        )}
+
+        <button
+          className={`text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
+          type="button"
+          onClick={handleSortByPrice}
+        >
+          Sort by price{" "}
+          {sortByPriceOrder
+            ? `(${sortByPriceOrder === "asc" ? "ASC" : "DESC"})`
+            : "DESC"}
+        </button>
+      </div>
+
+      {isLoading ? (
+        <LoadingSpinner />
       ) : (
         <div>
-          {orders.map((order) => (
-            <div key={order.id} className="border-b mb-4 pb-4">
-              <div className="w-full">
-                <div className="flex flex-col sm:flex-row justify-between">
-                  <div className="flex flex-col space-y-4 order-2 sm:order-1">
-                    <p className="font-semibold text-sm sm:text-base">
-                      Order ID: {order.id}
+          {orders.length === 0 ? (
+            <p>No orders found.</p>
+          ) : (
+            <div className="flex flex-col space-y-12 mt-10">
+              {orders.map((order) => (
+                <div
+                  key={order.id}
+                  className="border-b mb-4 py-4 px-4 rounded-lg shadow-sm bg-white"
+                >
+                  <div className="w-full">
+                    <div className="flex flex-col sm:flex-row justify-between">
+                      <div className="flex flex-col space-y-4 order-2 sm:order-1">
+                        <p className="font-semibold text-sm sm:text-base">
+                          Order ID: {order.id}
+                        </p>
+
+                        {order.status === "COMPLETED" && (
+                          <Rating
+                            orderId={order.id}
+                            initialRating={order.rating}
+                            updateRating={updateRating}
+                          />
+                        )}
+                      </div>
+
+                      <p className="font-semibold mt-4 text-sm sm:text-base sm:mt-0 order-1 sm:order-2">
+                        Order Date: {formatDate(order.createdAt)}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="">
+                    <div className=" mt-8 ">
+                      <table className="w-full divide-y divide-gray-300">
+                        <thead>
+                          <tr>
+                            <th
+                              scope="col"
+                              className="py-3.5  pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                            >
+                              Product
+                            </th>
+
+                            <th
+                              scope="col"
+                              className="px-3 py-3.5 text-right text-sm font-semibold text-gray-900 lg:table-cell"
+                            >
+                              Price
+                            </th>
+
+                            <th
+                              scope="col"
+                              className="px-3 py-3.5 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+                            >
+                              Interest
+                            </th>
+
+                            <th
+                              scope="col"
+                              className="pl-3 py-3.5 text-right text-sm font-semibold text-gray-900"
+                            >
+                              Total
+                            </th>
+                          </tr>
+                        </thead>
+
+                        <tbody className="divide-y divide-gray-200 bg-white">
+                          {order.items.map((item) => (
+                            <tr key={item.id}>
+                              <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                                {item.product.title}
+                              </td>
+
+                              <td className="px-3 py-4 text-sm text-right text-gray-500 lg:table-cell">
+                                {priceFormatter(item.product.price)}
+                              </td>
+
+                              <td className="px-3 py-4 text-sm text-right  text-gray-500 sm:table-cell">
+                                {priceFormatter(item.product.price * 0.15)}
+                              </td>
+
+                              <td className="px-3 py-4 text-sm  text-right  text-gray-500">
+                                ${priceFormatter(item.product.price * 1.15)}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+
+                    <p className="mt-4 font-semibold text-right">
+                      Total order: ${priceFormatter(order.totalPrice)}
                     </p>
 
-                    {order.status === "COMPLETED" && (
-                      <Rating
-                        orderId={order.id}
-                        initialRating={order.rating}
-                        updateRating={updateRating}
-                      />
+                    {order.status === "ACTIVE" && (
+                      <div className="flex mt-4 justify-end">
+                        <button
+                          id={order.id}
+                          className="px-4 py-2 bg-blue-500 text-white rounded"
+                          onClick={handleEditOrder}
+                        >
+                          Edit
+                        </button>
+
+                        <button
+                          id={order.id}
+                          className="px-4 py-2 bg-red-500 text-white rounded ml-2"
+                          onClick={handleCancelOrder}
+                          disabled={isCancellingOrder}
+                        >
+                          {isCancellingOrder ? "Cancelling..." : "Cancel"}
+                        </button>
+
+                        <button
+                          id={order.id}
+                          className="px-4 py-2 bg-green-500 text-white rounded ml-2"
+                          onClick={handlePayOrder}
+                          disabled={isPaymentLoading}
+                        >
+                          {isPaymentLoading ? "Paying..." : "Pay"}
+                        </button>
+                      </div>
+                    )}
+
+                    {isPaymentError && (
+                      <div className="mt-2 text-red-500">
+                        {isPaymentError.message}
+                      </div>
+                    )}
+
+                    {cancelOrderError && (
+                      <div className="mt-2 text-red-500">
+                        {cancelOrderError.message}
+                      </div>
                     )}
                   </div>
-
-                  <p className="font-semibold mt-4 text-sm sm:text-base sm:mt-0 order-1 sm:order-2">
-                    Order Date: {formatDate(order.createdAt)}
-                  </p>
                 </div>
-              </div>
-
-              <div className="">
-                <div className=" mt-8 ">
-                  <table className="w-full divide-y divide-gray-300">
-                    <thead>
-                      <tr>
-                        <th
-                          scope="col"
-                          className="py-3.5  pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                        >
-                          Product
-                        </th>
-
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-right text-sm font-semibold text-gray-900 lg:table-cell"
-                        >
-                          Price
-                        </th>
-
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-right text-sm font-semibold text-gray-900 sm:table-cell"
-                        >
-                          Interest
-                        </th>
-
-                        <th
-                          scope="col"
-                          className="pl-3 py-3.5 text-right text-sm font-semibold text-gray-900"
-                        >
-                          Total
-                        </th>
-                      </tr>
-                    </thead>
-
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                      {order.items.map((item) => (
-                        <tr key={item.id}>
-                          <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                            {item.product.title}
-                          </td>
-
-                          <td className="px-3 py-4 text-sm text-right text-gray-500 lg:table-cell">
-                            {priceFormatter(item.product.price)}
-                          </td>
-
-                          <td className="px-3 py-4 text-sm text-right  text-gray-500 sm:table-cell">
-                            {priceFormatter(item.product.price * 0.15)}
-                          </td>
-
-                          <td className="px-3 py-4 text-sm  text-right  text-gray-500">
-                            ${priceFormatter(item.product.price * 1.15)}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-
-                <p className="mt-4 font-semibold text-right">
-                  Total order: ${priceFormatter(order.totalPrice)}
-                </p>
-
-                {order.status === "ACTIVE" && (
-                  <div className="flex mt-4 justify-end">
-                    <button
-                      id={order.id}
-                      className="px-4 py-2 bg-blue-500 text-white rounded"
-                      onClick={handleEditOrder}
-                    >
-                      Edit
-                    </button>
-
-                    <button
-                      id={order.id}
-                      className="px-4 py-2 bg-red-500 text-white rounded ml-2"
-                      onClick={handleCancelOrder}
-                    >
-                      Cancel
-                    </button>
-
-                    <button
-                      id={order.id}
-                      className="px-4 py-2 bg-green-500 text-white rounded ml-2"
-                      onClick={handlePayOrder}
-                      disabled={isPaymentLoading ? true : false}
-                    >
-                      {isPaymentLoading ? "Paying..." : "Pay"}
-                    </button>
-                  </div>
-                )}
-
-                {isPaymentError && (
-                  <div className="mt-2 text-red-500">
-                    {isPaymentError.message}
-                  </div>
-                )}
-              </div>
+              ))}
             </div>
-          ))}
+          )}
         </div>
       )}
     </div>
