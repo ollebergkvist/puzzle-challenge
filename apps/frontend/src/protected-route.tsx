43602e76c9c336ae5cb689ac46eee1fabed8462f
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-20 10:47:37 -0700

    feat: add missing useeffect

--- apps/frontend/src/protected-route.tsx
+++ apps/frontend/src/protected-route.tsx
@@ -1,24 +1,23 @@
 // libs
 import { route } from "preact-router";
+import { useEffect } from "preact/hooks";
 import { useAuthContext } from "./context";
 import AsyncRoute from "preact-async-route";
 
 export const ProtectedRoute = ({ component: Component, path, ...rest }) => {
   const { isAuthenticated } = useAuthContext();
 
+  useEffect(() => {
+    if (!isAuthenticated) {
+      route("/login");
+    }
+  }, [isAuthenticated]);
+
   return (
     <AsyncRoute
       path={path}
       {...rest}
-      component={(props) =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          (() => {
-            route("/login");
-          })()
-        )
-      }
+      component={(props) => (isAuthenticated ? <Component {...props} /> : null)}
     />
   );
 };
