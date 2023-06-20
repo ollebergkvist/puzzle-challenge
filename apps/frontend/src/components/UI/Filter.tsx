a1165ad8939792d7776b86b22cbedca7f7badaec
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-19 17:07:49 -0700

    feat: add missing checked attribute

--- apps/frontend/src/components/UI/Filter.tsx
+++ apps/frontend/src/components/UI/Filter.tsx
@@ -56,6 +56,7 @@ export const Filter = ({
                   id={value}
                   type="checkbox"
                   value={value}
+                  checked={selectedValues.includes(value)}
                   className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                   onChange={(e) => {
                     const tempValue = e.target.checked
