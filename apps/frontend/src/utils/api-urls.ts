9615941a57823890e4f8220856fb955c4990672d
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-19 20:45:24 -0700

    feat: add url

--- apps/frontend/src/utils/api-urls.ts
+++ apps/frontend/src/utils/api-urls.ts
@@ -1,6 +1,7 @@
 export const getCartUrl = `${import.meta.env.VITE_API_URL}/cart/items`;
 export const createPaymentUrl = `${import.meta.env.VITE_API_URL}/orders/pay`;
 export const cancelOrderUrl = `${import.meta.env.VITE_API_URL}/orders/cancel`;
+export const getProductsUrl = `${import.meta.env.VITE_API_URL}/products`;
 
 export const createDynamicUrl = (orderId) => {
   const baseUrl = import.meta.env.VITE_API_URL;
