ab7795b6ec29c60c2dcce76db20dbf4880496a3a
Olle Bergkvist <hello@Ollebergkvist.com>
2023-06-20 10:58:03 -0700

    feat: add not found page

---
+++ apps/frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+export const NotFoundPage = () => {
+  return (
+    <div className="h-screen">
+      <h1>404 - Page Not Found</h1>
+
+      <p>The requested page could not be found.</p>
+    </div>
+  );
+};
--- apps/frontend/src/pages/index.ts
+++ apps/frontend/src/pages/index.ts
@@ -2,3 +2,4 @@ export * from "./CartPage";
 export * from "./HomePage";
 export * from "./LoginPage";
 export * from "./OrderDetailsPage";
+export * from "./NotFoundPage";
